97fe3ee6c853164f764e185f0ae7bf9d0e67d2b2
andrezito <134400745+andrezito007@users.noreply.github.com>
2024-11-10 21:30:26 -0300

    Add files via upload

--- forro/forro.js
+++ forro/forro.js
@@ -1,7 +1,7 @@
 let currentIndex = 0;
 
 function moveSlide(step) {
-    const slides = document.querySelectorAll('.carousel-item');
+    const slides = document.querySelectorAll('.carrossel-item');
     const totalSlides = slides.length;
     currentIndex += step;
 
@@ -11,7 +11,12 @@ function moveSlide(step) {
         currentIndex = 0;
     }
 
-    const carousel = document.querySelector('.carousel');
+    const carousel = document.querySelector('.carrossel');
     const offset = -currentIndex * 100;
     carousel.style.transform = `translateX(${offset}%)`;
 }
+
+// Automatiza a transição a cada 2,5 segundos (2500 milissegundos)
+setInterval(() => {
+    moveSlide(1); // Move para o próximo slide
+}, 2500);
